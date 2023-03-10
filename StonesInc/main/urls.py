e4408be1b847e04596721678c135cfc281422b2a
DanShinde <pravin113347@gmail.com>
2023-03-10 21:02:48 +0530

    Commit

--- StonesInc/main/urls.py
+++ StonesInc/main/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home, name='home'),
+    path('video_feed', views.videofeed, name='video_feed'),
     path('create_folder', views.create_folder, name='create_folder'),
     path('start_Photos', views.start_Photos, name='start_Photos'),
-    path('browser', views.browse_folder, name='browse_folder'),
-    path('StoredData/<path:path>', views.view_media, name='view_media'),
-    path('take_video', views.take_Video, name='take_Video'),
-    path('video_feed', views.video_feed, name='video_feed'),
-    path('start_stream', views.start_stream, name='start_stream'),
-    path('stop_stream', views.stop_stream, name='stop_stream'),
-    path('rec_on', views.rec_on, name='rec_on'),
-    path('rec_off', views.rec_off, name='rec_off'),
+    # path('browser', views.browse_folder, name='browse_folder'),
+    # path('StoredData/<path:path>', views.view_media, name='view_media'),
+    # path('take_video', views.take_Video, name='take_Video'),
+    # path('video_feed', views.video_feed, name='video_feed'),
+    # path('start_stream', views.start_stream, name='start_stream'),
+    # path('stop_stream', views.stop_stream, name='stop_stream'),
+    # path('rec_on', views.rec_on, name='rec_on'),
+    # path('rec_off', views.rec_off, name='rec_off'),
 ]
--- StonesInc/main/views.py
+++ StonesInc/main/views.py
@@ -4,13 +4,14 @@ from django.http import HttpResponse, StreamingHttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators import gzip
 from django.conf import settings
-from .funs import StepperMotor, create_options, capture_and_save_image, save_Video
+#from .funs import StepperMotor, create_options, capture_and_save_image, save_Video
 import os
 import cv2
+from .cams import VideoCam, gen
 
 # initial code to for motor
 GPIO_PIN_LIST = [5, 6, 13, 19]
-Motor = StepperMotor(GPIO_PIN_LIST)
+#Motor = StepperMotor(GPIO_PIN_LIST)
 # Motor.rotate(direction=True, degrees=-180)
 
 # Folder for storage
@@ -29,18 +30,30 @@ global out
 
 img_array = []
 
+@gzip.gzip_page
+def Home(request):
+    return render(request,'start.html')
 
-def home(request):
-    global cap
-    cap = cv2.VideoCapture(0)
-    cap.release()
-    FolderName = request.session.get('selectedFolder')
-    options = create_options(capturedFolder)
-    if request.method == "POST":
-        # getting input with name = fname in HTML form
-        FolderName = request.POST.get("FolderName")
-        return HttpResponse("Your name is "+ FolderName)
-    return render(request, "start.html", {"FolderN": FolderName, "options": options})
+def videofeed(request):
+    try:
+        cam = VideoCam()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:
+        pass
+
+
+
+# def home(request):
+#     global cap
+#     cap = cv2.VideoCapture(0)
+#     cap.release()
+#     FolderName = request.session.get('selectedFolder')
+#     options = create_options(capturedFolder)
+#     if request.method == "POST":
+#         # getting input with name = fname in HTML form
+#         FolderName = request.POST.get("FolderName")
+#         return HttpResponse("Your name is "+ FolderName)
+#     return render(request, "start.html", {"FolderN": FolderName, "options": options})
 
 
 def create_folder(request):
@@ -72,99 +85,99 @@ def start_Photos(request):
     global streaming
     streaming = False
     FolderName = request.session.get('selectedFolder')
-    capture_and_save_image(FolderName)
+    #capture_and_save_image(FolderName)
     return redirect("home")
 
 
-def browse_folder(request):
-    folder = 'static/StoredData'
-    files = os.listdir(folder)
-    return render(request, 'browse.html', {"files": files})
+# def browse_folder(request):
+#     folder = 'static/StoredData'
+#     files = os.listdir(folder)
+#     return render(request, 'browse.html', {"files": files})
 
 
-def view_media(request, path):
-    directory = '/static/StoredData'
-    imgfiles = os.listdir(f"static/StoredData/{path}")
-    imgfiles.sort()
-    return render(request, 'view_media.html', {"files": imgfiles, "path": path})
+# def view_media(request, path):
+#     directory = '/static/StoredData'
+#     imgfiles = os.listdir(f"static/StoredData/{path}")
+#     imgfiles.sort()
+#     return render(request, 'view_media.html', {"files": imgfiles, "path": path})
 
 
-def take_Video(request):
-    FolderName = request.session.get('selectedFolder')
-    save_Video(FolderName)
-    return redirect("home")
+# def take_Video(request):
+#     FolderName = request.session.get('selectedFolder')
+#     save_Video(FolderName)
+#     return redirect("home")
 
-def gen(rec):
-    # Open the video stream
-    global cap
-    cap = cv2.VideoCapture(0)
+# def gen(rec):
+#     # Open the video stream
+#     global cap
+#     cap = cv2.VideoCapture(0)
 
-    while True:
-        # Check the status of the streaming
-        if not streaming:
-            cap.release()
-            print("Not streaming")
-            break
+#     while True:
+#         # Check the status of the streaming
+#         if not streaming:
+#             cap.release()
+#             print("Not streaming")
+#             break
 
-        # Read a frame from the video stream
-        success, frame = cap.read()
-        if not success:
-            print("Error reading video stream")
-            break
-        frame = cv2.flip(frame, -1)
+#         # Read a frame from the video stream
+#         success, frame = cap.read()
+#         if not success:
+#             print("Error reading video stream")
+#             break
+#         frame = cv2.flip(frame, -1)
 
-        # Encode the frame as JPEG
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        if jpeg is None:
-            print("Error encoding frame as JPEG")
-            break
-        rec = recording
-        # If the recording is on, add the frame to the video
-        if rec:
-            img_array.append(frame)
-            #out.write(frame)
-            print(f"Recording {streaming} {len(img_array)}")
-        #print(streaming, recording, rec)
-        # Yield the JPEG frame
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+#         # Encode the frame as JPEG
+#         ret, jpeg = cv2.imencode('.jpg', frame)
+#         if jpeg is None:
+#             print("Error encoding frame as JPEG")
+#             break
+#         rec = recording
+#         # If the recording is on, add the frame to the video
+#         if rec:
+#             img_array.append(frame)
+#             #out.write(frame)
+#             print(f"Recording {streaming} {len(img_array)}")
+#         #print(streaming, recording, rec)
+#         # Yield the JPEG frame
+#         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 
-@gzip.gzip_page
-def video_feed(request):
-    return StreamingHttpResponse(gen(recording), content_type='multipart/x-mixed-replace; boundary=frame')
+# @gzip.gzip_page
+# def video_feed(request):
+#     return StreamingHttpResponse(gen(recording), content_type='multipart/x-mixed-replace; boundary=frame')
 
-def start_stream(request):
-  global streaming
-  streaming = True
-  print("Streaming started")
-  return HttpResponse('Stream started')
+# def start_stream(request):
+#   global streaming
+#   streaming = True
+#   print("Streaming started")
+#   return HttpResponse('Stream started')
 
-def stop_stream(request):
-  global streaming
-  streaming = False
-  print("Streaming Stopped")
-  return HttpResponse('Stream stopped')
+# def stop_stream(request):
+#   global streaming
+#   streaming = False
+#   print("Streaming Stopped")
+#   return HttpResponse('Stream stopped')
 
-def rec_on(request):
-  global recording
-  recording = True
-  print("Recording Started")
-  Motor.rotate(degrees=360, delay=0.01)
-  # Set the timer for 5 seconds
-  sleep(5)
-  recording = False
-  # Redirect to the rec_stop route to stop the recording
-  return redirect('rec_off')
+# def rec_on(request):
+#   global recording
+#   recording = True
+#   print("Recording Started")
+#   Motor.rotate(degrees=360, delay=0.01)
+#   # Set the timer for 5 seconds
+#   sleep(5)
+#   recording = False
+#   # Redirect to the rec_stop route to stop the recording
+#   return redirect('rec_off')
 
-def rec_off(request):
-  global recording
-  recording = False
-  FolderName = request.session.get('selectedFolder')
-  out = cv2.VideoWriter(f"static/StoredData/{FolderName}/exam1.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 24, (640, 480))
+# def rec_off(request):
+#   global recording
+#   recording = False
+#   FolderName = request.session.get('selectedFolder')
+#   out = cv2.VideoWriter(f"static/StoredData/{FolderName}/exam1.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 24, (640, 480))
   
-  for i, img in enumerate(img_array):
-    out.write(img)
-  out.release()
-  print(len(img_array))
-  img_array.clear()
-  print("Recording Stopped")
-  return HttpResponse('Recording stopped')
+#   for i, img in enumerate(img_array):
+#     out.write(img)
+#   out.release()
+#   print(len(img_array))
+#   img_array.clear()
+#   print("Recording Stopped")
+#   return HttpResponse('Recording stopped')
